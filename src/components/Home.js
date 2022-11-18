ae86e8eb72e55d52b8afb20149c14cd1184ff2e6
Arnaud293 <arnaudlt293@gmail.com>
2022-11-18 11:34:36 +0100

    Create Product component, implement it at home as static template, preparing style

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,26 @@ import React from 'react';
 
 // src
 import Background from '../assets/img/bg01.jpg';
+import Product from './Product';
 
 const Home = () => {
     return (
         <div className='home'>
             <div className="home__container">
                 <img src={Background} alt="" className="home__container-img" />
+
+                <div className="home__container_row">
+                    <Product />
+                    <Product />
+                </div>
+                <div className="home__container_row">
+                    <Product />
+                    <Product />
+                    <Product />
+                </div>
+                <div className="home__container_row">
+                    <Product />
+                </div>
             </div>
         </div>
     );
