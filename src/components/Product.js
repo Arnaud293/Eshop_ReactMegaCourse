b7b60c0aa928871701069db76b2e4e0d43dccc5d
Arnaud293 <arnaudlt293@gmail.com>
2022-11-22 10:58:44 +0100

    get props/number, implement product infos directly from data, miss img

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 // SRC
-import Product1 from '../../src/assets/img/productExample.jpg';
+import { productsData } from '../data/productsData';
 
-const Product = () => {
+const Product = ({productNumber}) => {
+
+    const [thisProduct] = useState(productsData[productNumber])
+
     return (
         <div className='product'>
             <div className="product__infos">
-                <p>Title</p>
-                <p className="product__infos-price">$$</p>
-                <div className="product__infos-rate">⭐</div>
+                <p>{thisProduct.title}</p>
+                <p className="product__infos-price">{thisProduct.price}$</p>
+                <div className="product__infos-rate">{thisProduct.rating}⭐</div>
             </div>
-            <img src={Product1} alt="tote-bag-black" className="product-img" />
+            <img src={thisProduct.img} alt="tote-bag-black" className="product-img" />
 
             <button type='button' className='basket-btn'>Add to basket</button>
         </div>
