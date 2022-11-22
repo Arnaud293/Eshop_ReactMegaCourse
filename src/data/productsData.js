ca57beeaffabe8ca9d8455ff5a634c7e8534aeba
Arnaud293 <arnaudlt293@gmail.com>
2022-11-22 10:33:32 +0100

    build inside data for more dynamic implementation

---
+++ src/data/productsData.js
@@ -0,0 +1,47 @@
+export const productsData = [
+    
+
+    {
+        id: 1,
+        title: "Singularity the empty bag. Maybe you can find the truth about our universe inside ...",
+        price: 240,
+        rating: 3,
+        img: "../../src/assets/img/singularity.jpg" ,
+    },
+    {
+        id: 2,
+        title: "This phone will never work but got a pretty cool design",
+        price: 170,
+        rating: 2,
+        img: "../../src/assets/img/martphone.jpg" ,
+    },
+    {
+        id: 3,
+        title: "Eat plutella, the first plutonian dust spread",
+        price: 4,
+        rating: 5,
+        img: "../../src/assets/img/plutella.jpg" ,
+    },
+    {
+        id: 4,
+        title: "Watta drink, watta oxygen, watta life, watta watta",
+        price: 21,
+        rating: 4,
+        img: "../../src/assets/img/wattab.jpg" ,
+    },
+    {
+        id: 5,
+        title: " Once you sit on it you will rule the world ",
+        price: 55,
+        rating: 4,
+        img: "../../src/assets/img/kingchair.jpg" ,
+    },
+    {
+        id: 6,
+        title: "Tired of solid and practical bags? This fragile sound carries nothing! Will you be up for the challenge of shopping with this...",
+        price: 12,
+        rating: 2,
+        img: "../../src/assets/img/productExample.jpg" ,
+    },
+
+]
