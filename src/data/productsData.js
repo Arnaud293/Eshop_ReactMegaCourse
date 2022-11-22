af4178f3cfd6c086d53c9ee3ff3aa075af91b70d
Arnaud293 <arnaudlt293@gmail.com>
2022-11-22 13:51:01 +0100

    fetch img with relative import cause of issue with others methods

--- src/data/productsData.js
+++ src/data/productsData.js
@@ -1,3 +1,10 @@
+import Plutella from '../../src/assets/img/plutella.jpg';
+import Singularity from '../../src/assets/img/singularity.jpg';
+import Martphone from '../../src/assets/img/martphone.jpg';
+import Wattab from '../../src/assets/img/wattab.jpg';
+import Kingchair from '../../src/assets/img/kingchair.jpg';
+import Bag from '../../src/assets/img/productExample.jpg';
+
 export const productsData = [
     
 
@@ -6,42 +13,48 @@ export const productsData = [
         title: "Singularity the empty bag. Maybe you can find the truth about our universe inside ...",
         price: 240,
         rating: 3,
-        img: "../../src/assets/img/singularity.jpg" ,
+        img: Singularity ,
+        alt: "empty-bag-picture"
     },
     {
         id: 2,
         title: "This phone will never work but got a pretty cool design",
         price: 170,
         rating: 2,
-        img: "../../src/assets/img/martphone.jpg" ,
+        img: Martphone ,
+        alt: "smartphone-picture"
     },
     {
         id: 3,
         title: "Eat plutella, the first plutonian dust spread",
         price: 4,
         rating: 5,
-        img: "../../src/assets/img/plutella.jpg" ,
+        img: Plutella ,
+        alt: "chocolate-spread-picture"
     },
     {
         id: 4,
         title: "Watta drink, watta oxygen, watta life, watta watta",
         price: 21,
         rating: 4,
-        img: "../../src/assets/img/wattab.jpg" ,
+        img: Wattab ,
+        alt: "gourd-picture"
     },
     {
         id: 5,
         title: " Once you sit on it you will rule the world ",
         price: 55,
         rating: 4,
-        img: "../../src/assets/img/kingchair.jpg" ,
+        img: Kingchair,
+        alt: "white-chair-picture"
     },
     {
         id: 6,
         title: "Tired of solid and practical bags? This fragile sound carries nothing! Will you be up for the challenge of shopping with this...",
         price: 12,
         rating: 2,
-        img: "../../src/assets/img/productExample.jpg" ,
+        img: Bag ,
+        alt: "cardboard-bag-picture"
     },
 
 ]
