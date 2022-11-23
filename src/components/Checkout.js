35fe2dc005e7d6fbe2935cd4607dff727f628b57
Arnaud293 <arnaudlt293@gmail.com>
2022-11-23 09:59:09 +0100

    create checkout component

---
+++ src/components/Checkout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header';
+
+const Checkout = () => {
+    return (
+
+        <div>
+            <Header />
+            
+        </div>
+    );
+};
+
+export default Checkout;
