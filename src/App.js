f2ba3e87c87a591765388f95cc445acdda0d24de
Arnaud293 <arnaudlt293@gmail.com>
2022-11-24 11:56:29 +0100

    put header in router

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import Home from './components/Home';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Checkout from './components/Checkout';
+import Header from './components/Header';
 
 const App = () => {
   return (
     <div className='App'>
       <BrowserRouter>
         <Routes>
-          <Route path='/checkout' element={<Checkout/>}/>
-          <Route path='/' element={<Home />}/>
+          <Route path='/checkout' element={<><Header/><Checkout/></>}/>
+          <Route path='/' element={<><Header/><Home /></>}/>
         </Routes>
       </BrowserRouter>
     </div>
