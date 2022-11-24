dacd9030a4ba8f14f9569c581161721c8b26f7fd
Arnaud293 <arnaudlt293@gmail.com>
2022-11-24 16:25:15 +0100

    add multiple checkoutProducts to get preview before mapping

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -8,9 +8,11 @@ const Checkout = () => {
 
         <div className='checkout'>
             <div className="checkout__left">
-                <img src="" alt="" className="checkout__ad" />
+                <img src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="" className="checkout__ad" />
                 <div>
                     <h2 className="checkout__title">Your shopping basket</h2>
+                    <CheckoutProducts />
+                    <CheckoutProducts />
                     <CheckoutProducts />
                 </div>
             </div>
