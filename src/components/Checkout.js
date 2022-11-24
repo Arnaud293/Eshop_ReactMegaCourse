fe0481c5419bd5b51c4d5c5a0488768a19a5aa6d
Arnaud293 <arnaudlt293@gmail.com>
2022-11-24 11:28:38 +0100

    import CheckoutProducts component

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import CheckoutProducts from './CheckoutProducts';
 import Header from './Header';
+import Subtotal from './Subtotal';
 
 const Checkout = () => {
     return (
 
-        <div>
+        <div className='checkout'>
             <Header />
+            <div className="checkout__left">
+                <img src="" alt="" className="checkout__ad" />
+                <div>
+                    <h2 className="checkout__title">Your shopping basket</h2>
+                    <CheckoutProducts />
+                </div>
+            </div>
+            <div className="checkout__right">
+                <Subtotal />
+            </div>
             
         </div>
     );
